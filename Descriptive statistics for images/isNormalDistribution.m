c53f48de70cad8456b242ac8518e30d4dc5df760
Andrew <Andrew>
2017-04-06 17:33:28 +0300

    Change first image and fix hypothesis testing

--- Descriptive statistics for images/isNormalDistribution.m
+++ Descriptive statistics for images/isNormalDistribution.m
@@ -1,8 +1,8 @@
 function result = isNormalDistribution( image )
 I1 = reshape(im2double(image),1,[]);
 if chi2gof(I1)
-    result = 'true';
-else
     result = 'false';
+else
+    result = 'true';
 end
 
--- Descriptive statistics for images/main.m
+++ Descriptive statistics for images/main.m
@@ -1,26 +1,26 @@
-marvelImage = imread('marvel.jpg');
+treesImage = imread('trees.jpg');
 foodImage = imread('food.jpg');
 
-fprintf('Marvel image statistics: \n');
-printDescriptiveStatistic(marvelImage);
+fprintf('Trees image statistics: \n');
+printDescriptiveStatistic(treesImage);
 fprintf('\n');
 
 fprintf('Food image statistics: \n');
 printDescriptiveStatistic(foodImage);
 fprintf('\n');
 
-marvelImageHistogram = imhist(rgb2gray(marvelImage));
+marvelImageHistogram = imhist(rgb2gray(treesImage));
 foodImageHistogram = imhist(rgb2gray(foodImage));
  
-figure, imhist(rgb2gray(marvelImage));
+figure, imhist(rgb2gray(treesImage));
 figure, imhist(rgb2gray(foodImage));
 
-fprintf('Correlation between images: %.2f\n', corr2(rgb2gray(marvelImage), rgb2gray(foodImage)));
+fprintf('Correlation between images: %.2f\n', corr2(rgb2gray(treesImage), rgb2gray(foodImage)));
 fprintf('Correlation between histograms: %.2f\n', corr2(marvelImageHistogram, foodImageHistogram));
 fprintf('\n');
 
-fprintf('Correlation between marvel image layers: \n');
-printCorrelationBetweenImageLayers(marvelImage);
+fprintf('Correlation between trees image layers: \n');
+printCorrelationBetweenImageLayers(treesImage);
 fprintf('\n');
 
 fprintf('Correlation between food image layers: \n');
@@ -28,5 +28,5 @@ printCorrelationBetweenImageLayers(foodImage);
 fprintf('\n');
 
 fprintf('Testing hypotheses about the normal distribution: \n');
-fprintf('\tFor marvel image: %s\n', isNormalDistribution(rgb2gray(marvelImage)));
+fprintf('\tFor trees image: %s\n', isNormalDistribution(rgb2gray(treesImage)));
 fprintf('\tFor food image: %s\n', isNormalDistribution(rgb2gray(foodImage)));
