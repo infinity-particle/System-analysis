46efc6eef0d714430bba65557b671f93757818d3
Andrew <zakharov-1997@yandex.by>
2017-04-20 00:33:26 +0300

    Fixed dictionaries parsing

--- Cluster analysis/getDictionary.m
+++ Cluster analysis/getDictionary.m
@@ -1,6 +1,6 @@
 function dictionary = getDictionary( fileName )
 file = fopen(fileName);
-dictionary = textscan(file, '%s', 'Delimiter', '-');
+dictionary = textscan(file, '%s %*[^\n]');
 dictionary = dictionary{1};
 end
 
