2d70fc6497e5baf1cce6e21f1b768fbc8c8bdd04
Andrew <zakharov-1997@yandex.by>
2017-04-20 10:30:50 +0300

    Useless code deleted

--- Cluster analysis/k_means.m
+++ Cluster analysis/k_means.m
@@ -1,32 +1,3 @@
-% m1 = importdata('./k-means/m1.txt');
-% m2 = importdata('./k-means/m2.txt');
-% 
-% words = { 'USA' 'Russia' };
-% 
-% clusterData = [m1, m2];
-% 
-% [indexes, cds] = kmeans(clusterData, 2);
-% 
-% hold on
-%  
-% for i = 1:2
-%     clust = find(indexes == i);
-%     
-%     x = clusterData(clust, 1);
-%     y = clusterData(clust, 2);
-%     
-%     plot(x, y, 'o');
-% end
-% 
-% plot(cds(:,1), cds(:,2), 'k*');
-% 
-% hold off
-% 
-% xlabel(words{1});
-% ylabel(words{2});
-% 
-% grid on
-
 load carbig.mat
 
 X = horzcat(MPG, Horsepower, Weight);
